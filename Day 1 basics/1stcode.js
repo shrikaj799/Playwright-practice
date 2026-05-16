eb8cc1ac8e0826ecdc4573510f4703f47d622de5
shrikaj799 <rajsurvase25@gmail.com>
2026-05-16 14:32:42 +0530

    Add addition logic in 1stcode.js

--- Day 1 basics/1stcode.js
+++ Day 1 basics/1stcode.js
@@ -2,3 +2,9 @@ console.log("Hello shree")
 
 console.log("Welcome to JavaScript")
 console.log(process.platform)
+
+write logic for addition of two numbers
+let num1 = 10;
+let num2 = 20;
+let sum = num1 + num2;
+console.log("The sum of " + num1 + " and " + num2 + " is: " + sum);
