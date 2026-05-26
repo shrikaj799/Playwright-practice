32450ee43c7f0a01d2dab26494f1df944055d3e8
shrikaj799 <rajsurvase25@gmail.com>
2026-05-26 19:02:59 +0530

    array commit

---
+++ Practice_PW/array_1_practice.js
@@ -0,0 +1,22 @@
+let s=[1,2,3,4,5]
+/*console.log(s[0]);
+console.log(s.length);
+console.log(s.at(-1));
+s.push(6);
+s.pop();
+for (t of s){
+    console.log(t);
+}
+
+//splice(start, delete count,  item to add)
+
+s.splice(1,1,10)
+
+for (t of s){
+    console.log(t);
+}
+*/
+for(let i=0;i<s.length;i++)
+{s
+    console.log(s[i])
+}
